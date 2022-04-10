e454058e00b5d225c58a63900cd4bdc4bb30086f
kanmpatel <kanm2618@gmail.com>
2022-04-10 12:36:01 +0100

    com-saucedemo

---
+++ src/LoginTesting/EmailAndPasswordWithChromeBrowser.java
@@ -0,0 +1,33 @@
+package LoginTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EmailAndPasswordWithChromeBrowser {
+    public static void main(String[] args) {
+        String baseUrl = "https://www.saucedemo.com/";
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //launch URL method
+        driver.get(baseUrl);
+        //print currentUrl
+        System.out.println("Current Url = "+ driver.getCurrentUrl());
+        //get the title of browser
+        String title = driver.getTitle();
+        System.out.println("my title is = "+title);
+        //find the email field element
+        WebElement emailField = driver.findElement(By.id("user-name"));
+        //sending email to email field elements
+        emailField.sendKeys("abc1234@gmail.com");
+        //find the password field element
+        WebElement passwordField = driver.findElement(By.name("password"));
+        //sending password to password field elements
+        passwordField.sendKeys("abcd1234");
+        //print the page source
+        System.out.println(driver.getPageSource());
+        //close the browser
+         driver.close();
+    }
+}
